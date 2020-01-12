acbfb1c587a0a2a8ada9e1e907063b846dd33214
leticiamendonca <leticia.mendonca68@gmail.com>
2020-01-12 21:57:59 +0530

    added heroku link

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/signin', {
+		fetch('https://floating-plains-95280.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
